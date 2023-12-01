3bb2c677925dd00c5bfc42cf3efd511870cc9e75
juan-perdomo30 <iori.hayashi.18@gmail.com>
2023-11-30 19:56:23 -0500

    Mi primer commit de Java con las actividades 1, 2 y 3 junto a otro ejercicio de constructor. (no pude hacer la actividad 4)

---
+++ src/main/java/co/com/ps/b6/actividad4.java
@@ -0,0 +1,50 @@
+package co.com.ps.b6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class actividad4 {
+
+    public static void main(String[] args) {
+        // Generar un número aleatorio del 1 al 100
+        Random random = new Random();
+        int numeroAdivinar = random.nextInt(100) + 1;
+
+        // Inicializar variables
+        Scanner scanner = new Scanner(System.in);
+        int intentos = 0;
+        int intentosMaximos = 10;
+        int intentoUsuario;
+
+        System.out.println("¡Adivina el número entre 1 y 100!");
+
+        while (intentos < intentosMaximos) {
+            System.out.print("Intento " + (intentos + 1) + ": ");
+            intentoUsuario = scanner.nextInt();
+
+            if (intentoUsuario == numeroAdivinar) {
+                System.out.println("¡Correcto! ¡Has adivinado el número!");
+                break;
+            } else {
+                mostrarPista(intentoUsuario, numeroAdivinar);
+            }
+
+            intentos++;
+        }
+
+        if (intentos == intentosMaximos) {
+            System.out.println("Lo siento, has agotado tus 10 intentos. El número era: " + numeroAdivinar);
+        }
+    }
+
+    // Método para mostrar pistas al usuario
+    private static void mostrarPista(int intentoUsuario, int numeroAdivinar) {
+        int diferencia = Math.abs(numeroAdivinar - intentoUsuario);
+
+        if (diferencia <= 10) {
+            System.out.println("Caliente!");
+        } else {
+            System.out.println("Frio...Frio!");
+        }
+    }
+}
